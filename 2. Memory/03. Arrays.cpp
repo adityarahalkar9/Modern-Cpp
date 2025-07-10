154cf8b7ce735f165472eb7e7d82e390bf24fa01
aditya <adityarahalkar9@gmail.com>
2025-07-10 17:00:23 +0530

    Some minor changes

--- 2. Memory/03. Arrays.cpp
+++ 2. Memory/03. Arrays.cpp
@@ -37,7 +37,6 @@ int arrays(){
 
 
 int twoDimensionalArrays(){
-
 	// Declare and inititialise a 2d array (matrix[no of rows][no of columns])
 	int matrix[3][4] = {
 		{1,2,3,4},
@@ -72,8 +71,7 @@ int twoDimensionalArrays(){
 
 
 
-int threeDimensionalArray() {
-
+int threeDimensionalArray(){
 	// Declare and initialize a 3D array: depth=2, rows=3, columns=2
 	int cube[2][3][2] = {
 		{ {1,  2}, {3,  4}, {5,  6} },
